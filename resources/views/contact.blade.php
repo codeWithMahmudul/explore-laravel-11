97ffc26dae57a88d47a87194163e1c8b890ab544
MdMahmudulHasanGit <mahmudul209307@gmail.com>
2024-05-02 05:27:17 +0600

    Layout added with tailwind component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Page</title>
-</head>
-
-<body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
+<x-layout>
+    <x-slot:heading>Contact</x-slot:heading>
     <h2>Contact Page</h2>
-</body>
-
-</html>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-slot:heading>Home</x-slot:heading>
     <h2>Home Page</h2>
 </x-layout>
