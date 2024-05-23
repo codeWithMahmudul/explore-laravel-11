a81e558a0552f8d455b79f5bcf91f0798c9f6419
MdMahmudulHasanGit <mahmudul209307@gmail.com>
2024-05-23 18:10:06 +0600

    Feature: Form and form validation added

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -5,7 +5,7 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <article>
-                <a href="/job/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
                     <div>{{ $job->employer->name }}</div>
                    <div>
                     {{ $job['title'] }}: Pays {{ $job['salary'] }} per year.
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
@@ -7,16 +8,37 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('jobs/create', function (){
+    return view('jobs.create');
+});
+
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/job/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', compact('job'));
+    return view('jobs.show', compact('job'));
+});
+
+Route::post('/jobs', function (){
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'numeric'],
+    ], [
+        'title.required' => 'Job title is required',
+        'salary.required' => 'Job salary is required',
+    ]);
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
 
 
