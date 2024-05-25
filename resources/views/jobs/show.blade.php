326f48d22dcfa6098d86426f6f2c7f733c0b081b
MdMahmudulHasanGit <mahmudul209307@gmail.com>
2024-05-25 10:04:38 +0600

    Feature: Job Update Delete Option added

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,7 +1,11 @@
 <x-layout>
     <x-slot:heading>Job</x-slot:heading>
-    <h2>{{ $job['title'] }}</h2>
+    <h2>{{ $job->title }}</h2>
     <p>
-        This job pays {{ $job['salary'] }} per year.
+        This job pays {{ $job->salary }} per year.
+    </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit</x-button>
     </p>
 </x-layout>
