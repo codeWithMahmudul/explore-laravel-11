4d5eae6c67cd2b757a614171392841a108ea2fdc
codeWithMahmudul <mahmudul209307@gmail.com>
2024-05-26 10:57:37 +0600

    Test changed 2

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-// Work on it
+// Work on it ss
 require __DIR__.'/auth.php';
