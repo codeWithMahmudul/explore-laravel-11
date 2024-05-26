ecc8cce8398ee50daa73537aa0cad0c3da9b69a3
codeWithMahmudul <mahmudul209307@gmail.com>
2024-05-26 10:59:50 +0600

    Auth added

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 Route::resource('jobs', JobController::class);
+//Auth added in other branch
 
 Route::view('/contact', 'contact')->name('contact');
 
