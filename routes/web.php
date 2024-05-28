6c7299c0aa950215017713960a77a69d7ae2e920
codeWithMahmudul <mahmudul209307@gmail.com>
2024-05-28 09:25:59 +0600

    Auth: Login Registration layout added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
@@ -8,3 +10,9 @@ Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact')->name('contact');
 
+// Auth
+Route::get('register', [RegisteredUserController::class, 'create']);
+Route::post('register', [RegisteredUserController::class, 'store']);
+
+Route::get('login', [SessionController::class, 'create'])->name('login');
+Route::post('login', [SessionController::class, 'store'])->name('store');
